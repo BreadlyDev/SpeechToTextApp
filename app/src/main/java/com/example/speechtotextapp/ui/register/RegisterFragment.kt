6b0cd20d08cc2d38c35662a571b2d6346a837d8a
BreadDev <breadydev@gmail.com>
2024-04-17 18:47:52 +0600

    Fixed

--- app/src/main/java/com/example/speechtotextapp/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/example/speechtotextapp/ui/register/RegisterFragment.kt
@@ -1,10 +1,11 @@
-package com.example.speechtotextapp
+package com.example.speechtotextapp.ui.register
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.speechtotextapp.R
 
 class RegisterFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
