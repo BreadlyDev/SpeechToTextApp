7903db07cc23c0a91f1b3a03ac5d36868bcf0f06
2pizzzza <amit.kurbanov.123.com@gmail.com>
2024-04-17 16:51:54 +0600

    added xml signup and login

--- app/src/main/java/com/example/speechtotextapp/sttApi/RetrofitClient.kt
+++ app/src/main/java/com/example/speechtotextapp/sttApi/RetrofitClient.kt
@@ -1,5 +1,6 @@
 package com.example.speechtotextapp.sttApi
 
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -7,6 +8,9 @@ object RetrofitClient {
     private const val BASE_URL = "http://https://asr.ulut.kg/api/receive_data"
 
     private val retrofit: Retrofit by lazy {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
