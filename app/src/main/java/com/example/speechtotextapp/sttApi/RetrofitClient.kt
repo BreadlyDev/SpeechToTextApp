0881a15f1692e94ae24e798cefdf88e632892e91
2pizzzza <amit.kurbanov.123.com@gmail.com>
2024-04-17 17:57:39 +0600

    interceptor

--- app/src/main/java/com/example/speechtotextapp/sttApi/RetrofitClient.kt
+++ app/src/main/java/com/example/speechtotextapp/sttApi/RetrofitClient.kt
@@ -1,13 +1,22 @@
 package com.example.speechtotextapp.sttApi
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 //    private const val BASE_URL = "https://asr.ulut.kg/api/receive_data"
-    private const val BASE_URL = "http://0.0.0.0:8000"
+    private const val BASE_URL = "http://192.168.54.19:8000"
 
     private val retrofit: Retrofit by lazy {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
